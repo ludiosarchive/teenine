a5159792775404f9116dee0a74de923089dba9fb
Ivan Kozik <ivan@ludios.org>
2015-07-01 23:30:32 +0000

    Map uio jlk keys to number pad keys

--- index.js
+++ index.js
@@ -30,6 +30,33 @@ for (const number of Object.keys(numberToLetters)) {
 assert.strictEqual(Object.keys(numberToLetters).length, 8);
 assert.strictEqual(Object.keys(letterToNumbers).length, 26);
 
+// Some users have no number pad, so let them use
+// the keys below the 789 keys in their number row.
+const numberToPhysicalKeys = {
+	7: "7",
+	8: "8",
+	9: "9",
+
+	4: "u4",
+	5: "i5",
+	6: "o6",
+
+	1: "j1",
+	2: "k2",
+	3: "l3"
+}
+
+const physicalKeyToNumber = Object.create(null);
+
+for (const number of Object.keys(numberToPhysicalKeys)) {
+	for (const letter of numberToPhysicalKeys[number]) {
+		physicalKeyToNumber[letter] = number;
+	}
+}
+
+assert.strictEqual(Object.keys(numberToPhysicalKeys).length, 9);
+assert.strictEqual(Object.keys(physicalKeyToNumber).length, 9 + 6);
+
 console.log(
 `Upside-down T9 IME
 
